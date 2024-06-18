4a0fd87e70123770b597af4527b4f8a896528f21
Peter Kirmeier <topeterk@users.noreply.github.com>
2024-06-18 19:12:29 +0200

    Fix architecture of build script

--- __build_osx_arm64.bat
+++ __build_osx_arm64.bat
@@ -1,2 +1,2 @@
-dotnet publish SystemTrayMenu.csproj --configuration Debug --runtime osx-x64 --self-contained false --framework net8.0
+dotnet publish SystemTrayMenu.csproj --configuration Debug --runtime osx-arm64 --self-contained false --framework net8.0
 pause
